39400ca91332e1997b48bf287420057f5f186cdd
Gabriel Araujo <araujo.gca214@gmail.com>
2022-07-23 17:27:05 -0300

    forca.js finalizado e comentado

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,41 +1,68 @@
 class Forca {
+  letraChutadaRepetida(letra) {
+    for (let i = 0; i < this.letrasChutadas.length; i++) {
+      if (letra == this.letrasChutadas[i]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   chutar(letra) {
-    if (letra.length() == 1) {
-      for (let i = 0; i < this.palavraSecreta.length; i++) {
-        if (this.palavraSecreta[i] == letra) {
-          letrasChutadas.push(letra);
-          palavra[i] == letra;
-        } else {
-          letrasChutadas.push(letra);
-          vidas--;
+    if (letra.length == 1) {
+      if (!this.letraChutadaRepetida(letra)) {
+        this.letrasChutadas.push(letra);
+        // O loop checa se a letra chutada é igual a algum caractere da palavraSecreta, e se sim, ele troca o espaço "_" da array palavra pela letra, e chuteCerto se torna verdadeiro
+        let chuteCerto = false;
+        for (let i = 0; i <= this.palavraSecreta.length; i++) {
+          if (this.palavraSecreta[i] == letra) {
+            this.palavra[i] = letra;
+            chuteCerto = true;
+          }
+        }
+        if (!chuteCerto) {
+          this.vidas--;
         }
       }
     }
   }
 
   buscarEstado() {
-    if (this.buscarDadosDoJogo().vidas == 0) {
+    if (this.vidas <= 0) {
       return "perdeu";
-    } else if (this.palavraSecreta == palavra) {
-      return "ganhou";
-    } else {
-      return "aguardando chute";
     }
+    // O loop adiciona 1 para cada letra igual que estiver presente em ambas as variáveis palavraSecreta e palavra, e se a quantidade de letrasCertas for igual ao tamanho da palavraSecreta (ou seja, se a palavraSecreta e palavra forem iguais), o método retorna ao usuário que ele ganhou
+    let letrasCertas = 0;
+    for (let i = 0; i < this.palavraSecreta.length; i++) {
+      if (this.palavraSecreta[i] == this.palavra[i]) {
+        letrasCertas++;
+      }
+    }
+    if (letrasCertas == this.palavraSecreta.length) {
+      return "ganhou";
+    }
+    return "aguardando chute";
   } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
   buscarDadosDoJogo() {
-    return {
-      letrasChutadas: [], // Deve conter todas as letras chutadas
-      vidas: 6, // Quantidade de vidas restantes
-      palavra: [], // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
-    };
+    // Para cada caractere da palavraSecreta, é adicionado um "_" na array palavra
+    for (let i = 0; i < this.palavraSecreta.length; i++) {
+      if (this.palavra[i] == undefined) {
+        this.palavra[i] = "_";
+      }
+    }
+    return [
+      this.letrasChutadas, // Deve conter todas as letras chutadas
+      this.vidas, // Quantidade de vidas restantes
+      this.palavra, // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+    ];
   }
 
   constructor(palavraSecreta) {
     this.palavraSecreta = palavraSecreta;
-    this.palavraSecreta.forEach((element) => {
-      palavra.push("_");
-    });
+    this.letrasChutadas = [];
+    this.palavra = [];
+    this.vidas = 6;
   }
 }
 
